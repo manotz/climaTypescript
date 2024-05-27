0cfeba2384fff591d985aa3874bece3eae16fd4b
manotz <guillertalacoro.az711224@gmail.com>
2024-05-27 08:03:32 -0600

    corrigiendo errores

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -66,7 +66,7 @@ export default function useWeather(){
         const appId = import.meta.env.VITE_API_KEY;
 
         try {
-            const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
+            const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
 
             const {data} = await axios.get(geoUrl);
 
